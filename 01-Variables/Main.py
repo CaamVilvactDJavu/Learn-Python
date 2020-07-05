233186f5316b9abffde84b282b8ca7872eea5339
CaamVilvactDJavu <caam.vilvact11@gmail.com>
2020-07-05 09:36:14 +0700

    New Project Python

---
+++ 01-Variables/Main.py
@@ -0,0 +1,18 @@
+import turtle
+
+# Square
+qazi_turtle = turtle.Turtle()
+
+
+def square():
+    qazi_turtle.forward(100)
+    qazi_turtle.right(90)
+    qazi_turtle.forward(100)
+    qazi_turtle.right(90)
+    qazi_turtle.forward(100)
+    qazi_turtle.right(90)
+    qazi_turtle.forward(100)
+
+
+square()
+qazi_turtle.forward(200)
---
+++ 03-Calendar/Main.py
@@ -0,0 +1,4 @@
+import calendar
+
+year = (calendar.calendar(2020))
+print(year)
