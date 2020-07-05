56e7e7a79c48095a7db4b1bf55a7ebbf926d68aa
CaamVilvactDJavu <caam.vilvact11@gmail.com>
2020-07-05 12:02:17 +0700

    Tuples

---
+++ 11-Tuples/Main.py
@@ -0,0 +1,16 @@
+person1 = ("Caam", 18, "Coca Cola")
+person2 = ("Vilvact", 19, "Fanta")
+
+people = [person1, person2]
+
+# name, age, favdrink = person1
+
+# print(name)
+# print(age)
+# print(favdrink)
+
+for name, age, favdrink in people:
+    print(name)
+    print(age)
+    print(favdrink)
+    print("=====================================")
