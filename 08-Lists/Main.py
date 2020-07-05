e65dbf60adbc4a07bc68bdd21a77f57aa4f90ebd
CaamVilvactDJavu <caam.vilvact11@gmail.com>
2020-07-05 10:57:15 +0700

    Lists

---
+++ 08-Lists/Main.py
@@ -0,0 +1,39 @@
+# List
+listtwo = [1, "Hello", 'World', 0.5, True, [1, 2, 3]]
+
+print(listtwo[0])
+print(listtwo[1])
+print(listtwo[2])
+print(listtwo[3])
+print(listtwo[4])
+print(listtwo[5])
+
+# Names
+names = ["Caam", "Vilvact", "D'Javu"]
+print(names)
+names.insert(1, "Guys")
+print(names)
+
+names = ["Caam", "Vilvact", "D'Javu"]
+print(names)
+names.append("Gans")
+print(names)
+
+names = ["Caam", "Vilvact", "D'Javu"]
+print(names)
+names.remove("D'Javu")
+print(names)
+
+names = ["Caam", "Vilvact", "D'Javu"]
+print(names)
+names.reverse()
+print(names)
+
+# Number
+numbers = [2, 10, 4, 5, 300, 220]
+print(numbers)
+numbers.sort()
+print(numbers)
+
+for number in numbers:
+    print(number)
