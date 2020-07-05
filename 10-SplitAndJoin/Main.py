3c10433980ba5c25791c6207db6c84391b2c8caf
CaamVilvactDJavu <caam.vilvact11@gmail.com>
2020-07-05 11:53:20 +0700

    Split And Join

---
+++ 10-SplitAndJoin/Main.py
@@ -0,0 +1,26 @@
+problems = 'broke, pale, short, nerdy'
+print(problems)
+l = problems.split(", ")
+print(l)
+
+print("=====================================")
+problems = 'broke, pale, short, nerdy'
+print(problems)
+l = problems.split("short")
+print(l)
+
+print("=====================================")
+problems = 'broke, pale, short, nerdy'
+print(problems)
+l = problems.split(", ")
+print(l)
+joined = ' and '.join(l)
+print(joined)
+
+print("=====================================")
+problems = 'broke, pale, short, nerdy'
+print(problems)
+l = problems.split(", ")
+print(l)
+csv = ' , '.join(l)
+print(csv)
