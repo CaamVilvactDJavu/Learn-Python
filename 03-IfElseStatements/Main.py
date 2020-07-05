63e4670325b1ac44f0af4b8a4fbf6c269f265797
CaamVilvactDJavu <caam.vilvact11@gmail.com>
2020-07-05 09:55:58 +0700

    If Else Statements

---
+++ 03-IfElseStatements/Main.py
@@ -0,0 +1,26 @@
+import turtle
+
+# Square
+qazi_turtle = turtle.Turtle()
+
+
+def square():
+    qazi_turtle.forward(100)
+    qazi_turtle.right(90)
+    qazi_turtle.forward(100)
+    qazi_turtle.right(90)
+    qazi_turtle.forward(100)
+    qazi_turtle.right(90)
+    qazi_turtle.forward(100)
+
+
+# square()
+# qazi_turtle.forward(200)
+
+elephant_weight = 3000
+ant_weight = 0.1
+
+if elephant_weight < ant_weight:
+    square()
+else:
+    qazi_turtle.forward(300)
